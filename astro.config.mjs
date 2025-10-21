f9d1a7c2bbbf44ce8d5d563fc92387e341023c84
Abdur Rahman Abul Hossain <50491150+Rifat1@users.noreply.github.com>
2025-10-21 16:36:47 -0230

    Update astro config base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "https://rifat1.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
